6d441fb782cfe4cc3c815eccfc0a34430590c238
Vladislav Kurenkov <v.kurenkov@innopolis.ru>
2019-07-19 18:10:07 +0300

    Add viscosity to cabel (data model and simulation)

--- example.py
+++ example.py
@@ -7,16 +7,16 @@ from copy          import deepcopy
 # Let's construct a robot of two rods and one cable
 rod1  = Rod(mass=1, inertia=np.eye(1), length=5, state=RodState())
 rod2  = Rod(mass=1, inertia=np.eye(1), length=5, state=RodState(r=rod1.get_state().r + np.array([0, 0, 10])))
-cab1  = Cable(end_point1=rod1.get_endpoint_a(), end_point2=rod2.get_endpoint_a(), stiffness=10, unstretched_length=0.1)
-cab2  = Cable(end_point1=rod1.get_endpoint_b(), end_point2=rod2.get_endpoint_b(), stiffness=10, unstretched_length=0.1)
+cab1  = Cable(end_point1=rod1.get_endpoint_a(), end_point2=rod2.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab2  = Cable(end_point1=rod1.get_endpoint_b(), end_point2=rod2.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
 
 robot = TensegrityRobot()
 robot.add_rods([rod1, rod2])
 robot.add_cables([cab1, cab2])
 print("First rod cables vectors")
-print(rod1.get_cables_vectors())
+print(rod1.get_cables_states())
 print("Second rod cables vectors")
-print(rod2.get_cables_vectors())
+print(rod2.get_cables_states())
 
 # Visualise the initial state of the robot
 rob_vis.plot(robot)
--- model.py
+++ model.py
@@ -58,13 +58,13 @@ class Rod:
         return cables
     
     # Geometrical properties
-    def get_cables_vectors(self, state=None):
+    def get_cables_states(self, state=None):
         """
         Returns all cables for this rod [(origin point, direction)], such that origin point is on this rod.
         Direction is not normalized.
         """
-        vectors = self.get_endpoint_a().get_cables_vectors(state)
-        vectors.extend(self.get_endpoint_b().get_cables_vectors(state))
+        vectors = self.get_endpoint_a().get_cables_states(state)
+        vectors.extend(self.get_endpoint_b().get_cables_states(state))
         return vectors
 
     # Physical properties
@@ -83,12 +83,16 @@ class Rod:
         if state is None:
             state = self.get_state()
 
-        cable_vectors = self.get_cables_vectors(state)
+        cable_vectors = self.get_cables_states(state)
         force         = np.zeros(3,)
         torque        = np.zeros(3,)
-        for (cable, src_pos, direction) in cable_vectors:
+        for (cable, src_pos, direction, out_velocity, in_velocity) in cable_vectors:
             l   = np.linalg.norm(direction) - cable.get_unstretched_length()
-            f   = cable.get_stiffness() * l * (direction / np.linalg.norm(direction))
+            v   = in_velocity - out_velocity
+            
+            f_d = cable.get_viscosity() * v
+            f_p = cable.get_stiffness() * l * (direction / np.linalg.norm(direction))
+            f   = f_d + f_p
             tau = np.cross(src_pos, f)
 
             force  += f
@@ -107,6 +111,7 @@ class Rod:
         ddr           = force / self.get_mass()
         # Angular acceleration
         dw = np.linalg.solve(self.get_inertia(), (torque - np.cross(state.w, self.get_inertia() * state.w)))
+        dw = dw.reshape((3,))
 
         return ddr, dw
 
@@ -151,7 +156,6 @@ class EndPoint:
     def is_a(self):
         return self.get_rod().get_endpoint_a() is self
 
-    # Geometrical properties
     def get_position(self, state=None):
         # Given position, orientation, and length -- compute the edges positions
         displacement = self.get_rod().get_length() / 2
@@ -162,7 +166,13 @@ class EndPoint:
             state = self.get_rod().get_state()
 
         return state.r + state.q.apply([displacement, 0, 0])
-    def get_cables_vectors(self, state=None):
+    def get_velocity(self, state=None):
+        if state is None:
+            state = self.get_rod().get_state()
+
+        return state.dr + np.cross(self.get_position(state) - state.r, state.w)
+     
+    def get_cables_states(self, state=None):
         """
         Returns all cables for this EndPoint [(origin point, direction)], such that origin point is on this EndPoint.
         Direction is not normalized. 
@@ -170,22 +180,25 @@ class EndPoint:
         if state is None:
             state = self.get_rod().get_state()
 
-        vectors = []
-        src_pos = self.get_position(state)
+        vectors  = []
+        src_pos  = self.get_position(state)
+        velocity = self.get_velocity(state)
         for cable in self._cabels:
             other_endpoint = cable.get_endpoint(0)
             if other_endpoint is self:
                 other_endpoint = cable.get_endpoint(1)
+            other_velocity = other_endpoint.get_velocity()
 
-            vectors.append((cable, src_pos, other_endpoint.get_position() - src_pos))
+            vectors.append((cable, src_pos, other_endpoint.get_position() - src_pos, velocity, other_velocity))
 
         return vectors
 
 class Cable:
-    def __init__(self, end_point1, end_point2, stiffness, unstretched_length):
+    def __init__(self, end_point1, end_point2, stiffness, unstretched_length, viscosity):
         self._end_points         = [end_point1, end_point2]
         self._stiffness          = stiffness
         self._unstretched_length = unstretched_length
+        self._viscosity          = viscosity
 
         end_point1.add_cable(self)
         end_point2.add_cable(self)
@@ -201,3 +214,5 @@ class Cable:
         return self._stiffness
     def get_unstretched_length(self):
         return self._unstretched_length
+    def get_viscosity(self):
+        return self._viscosity
