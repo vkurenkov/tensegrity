01d0155b80e3fae75b25e620201e1c7a8924314b
Vladislav Kurenkov <v.kurenkov@innopolis.ru>
2019-07-22 12:25:40 +0300

    add more sophisticated example; change visualisation
    - visualisation: add labeling
    - visualisation: make all rods of the same color

--- example.py
+++ example.py
@@ -1,22 +1,55 @@
 import numpy         as np
 import visualisation as rob_vis
 
-from model         import Rod, RodState, Cable, TensegrityRobot
-from copy          import deepcopy
+from model                   import Rod, RodState, Cable, TensegrityRobot
+from copy                    import deepcopy
+from scipy.spatial.transform import Rotation
 
-# Let's construct a robot of two rods and one cable
-rod1  = Rod(mass=1, inertia=np.eye(1), length=5, state=RodState())
-rod2  = Rod(mass=1, inertia=np.eye(1), length=5, state=RodState(r=rod1.get_state().r + np.array([0, 0, 10])))
-cab1  = Cable(end_point1=rod1.get_endpoint_a(), end_point2=rod2.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
-cab2  = Cable(end_point1=rod1.get_endpoint_b(), end_point2=rod2.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+LENGTH = 5.0
+OFFSET = LENGTH/4.0
+
+rod1  = Rod(mass=1, inertia=np.eye(1), length=LENGTH, state=RodState(r=np.array([0, 0, -OFFSET]), q=Rotation.from_euler("xyz", [0, 0, 0], degrees=True)))
+rod2  = Rod(mass=1, inertia=np.eye(1), length=LENGTH, state=RodState(r=np.array([0, 0, OFFSET]),  q=Rotation.from_euler("xyz", [0, 0, 0], degrees=True)))
+rod3  = Rod(mass=1, inertia=np.eye(1), length=LENGTH, state=RodState(r=np.array([-OFFSET, 0, 0]), q=Rotation.from_euler("xyz", [0, 0, 90], degrees=True)))
+rod4  = Rod(mass=1, inertia=np.eye(1), length=LENGTH, state=RodState(r=np.array([OFFSET, 0, 0]),  q=Rotation.from_euler("xyz", [0, 0, 90], degrees=True)))
+rod5  = Rod(mass=1, inertia=np.eye(1), length=LENGTH, state=RodState(r=np.array([0, -OFFSET, 0]), q=Rotation.from_euler("xyz", [0, 90, 0], degrees=True)))
+rod6  = Rod(mass=1, inertia=np.eye(1), length=LENGTH, state=RodState(r=np.array([0, OFFSET, 0]),  q=Rotation.from_euler("xyz", [0, 90, 0], degrees=True)))
+
+cab1  = Cable(end_point1=rod1.get_endpoint_a(), end_point2=rod3.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab2  = Cable(end_point1=rod1.get_endpoint_a(), end_point2=rod3.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab3  = Cable(end_point1=rod1.get_endpoint_a(), end_point2=rod6.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab4  = Cable(end_point1=rod1.get_endpoint_a(), end_point2=rod5.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab5  = Cable(end_point1=rod1.get_endpoint_b(), end_point2=rod4.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab6  = Cable(end_point1=rod1.get_endpoint_b(), end_point2=rod4.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab7  = Cable(end_point1=rod1.get_endpoint_b(), end_point2=rod6.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab8  = Cable(end_point1=rod1.get_endpoint_b(), end_point2=rod5.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+
+cab9  = Cable(end_point1=rod5.get_endpoint_b(), end_point2=rod3.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab10 = Cable(end_point1=rod5.get_endpoint_b(), end_point2=rod4.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab11  = Cable(end_point1=rod6.get_endpoint_b(), end_point2=rod3.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab12 = Cable(end_point1=rod6.get_endpoint_b(), end_point2=rod4.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab13  = Cable(end_point1=rod3.get_endpoint_a(), end_point2=rod2.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab14 = Cable(end_point1=rod3.get_endpoint_a(), end_point2=rod5.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab15  = Cable(end_point1=rod4.get_endpoint_a(), end_point2=rod2.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab16 = Cable(end_point1=rod4.get_endpoint_a(), end_point2=rod5.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab17  = Cable(end_point1=rod3.get_endpoint_b(), end_point2=rod2.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab18 = Cable(end_point1=rod3.get_endpoint_b(), end_point2=rod6.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab19  = Cable(end_point1=rod4.get_endpoint_b(), end_point2=rod6.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab20 = Cable(end_point1=rod4.get_endpoint_b(), end_point2=rod2.get_endpoint_b(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab21  = Cable(end_point1=rod2.get_endpoint_a(), end_point2=rod6.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab22 = Cable(end_point1=rod2.get_endpoint_a(), end_point2=rod5.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab23  = Cable(end_point1=rod2.get_endpoint_b(), end_point2=rod6.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+cab24 = Cable(end_point1=rod2.get_endpoint_b(), end_point2=rod5.get_endpoint_a(), stiffness=10, unstretched_length=0.1, viscosity=10)
+
 
 robot = TensegrityRobot()
-robot.add_rods([rod1, rod2])
-robot.add_cables([cab1, cab2])
-print("First rod cables vectors")
-print(rod1.get_cables_states())
-print("Second rod cables vectors")
-print(rod2.get_cables_states())
+robot.add_rods([rod1, rod2, rod3, rod4, rod5, rod6])
+robot.add_cables([cab1, cab2, cab3, cab4, cab5, cab6, cab7, cab8, cab9, cab10, cab11, cab12])
+robot.add_cables([cab13, cab14, cab15, cab16, cab17, cab18, cab19, cab20, cab21, cab22, cab23, cab24])
+# print("First rod cables vectors")
+# print(rod1.get_cables_states())
+# print("Second rod cables vectors")
+# print(rod2.get_cables_states())
 
 # Visualise the initial state of the robot
 rob_vis.plot(robot)
--- visualisation.py
+++ visualisation.py
@@ -8,18 +8,22 @@ def plot(robot):
     fig   = plt.figure()
     ax    = Axes3D(fig)
     ax.set_title("Visualisation of the robot state.")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
 
     # Draw rods
-    for rod in robot.get_rods():
+    for i, rod in enumerate(robot.get_rods()):
         pos1 = rod.get_endpoint_a().get_position()
         pos2 = rod.get_endpoint_b().get_position()
 
-        ax.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], [pos1[2], pos2[2]], linewidth=3)
+        ax.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], [pos1[2], pos2[2]], linewidth=3, c="red")
+        ax.text(pos2[0] + (pos1[0] - pos2[0]) / 2, pos2[1] + (pos1[1] - pos2[1]) / 2, pos2[2] + (pos1[2] - pos2[2]) / 2, "rod_{}".format(i+1))
 
     # Draw cables
     for cable in robot.get_cables():
         pos1 = cable.get_endpoint(0).get_position()
         pos2 = cable.get_endpoint(1).get_position()
 
-        ax.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], [pos1[2], pos2[2]], c="black")
+        ax.plot([pos1[0], pos2[0]], [pos1[1], pos2[1]], [pos1[2], pos2[2]], c="black", linewidth=0.5)
     plt.show()
