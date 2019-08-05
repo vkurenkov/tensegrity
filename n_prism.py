7c262228caa49860d4dedf833b1369ac7f058a19
Vladislav Kurenkov <v.kurenkov@innopolis.ru>
2019-08-05 15:03:28 +0300

    add prism configuration and example for an arbitrary N

---
+++ n_prism.py
@@ -0,0 +1,13 @@
+import visualisation          as rob_vis
+import configurations.n_prism as n_prism
+
+from model          import  run_simulation
+
+# Build a robot
+robot = n_prism.build(n=6)
+
+rob_vis.plot_cur_state(robot)
+hist_states = run_simulation(robot, time=3, dt=0.005)
+rob_vis.plot_cur_state(robot)
+rob_vis.plot_com_graphs(hist_states)
+
